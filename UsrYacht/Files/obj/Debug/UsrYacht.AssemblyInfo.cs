925ee6cffe04db54d0d0b145b5a89c6902da3804
Behnaz Abdul Razaq <behnazabdulrazaaq845@gmail.com>
2026-04-07 16:47:09 +0300

    small demo change

--- UsrYacht/Files/obj/Debug/UsrYacht.AssemblyInfo.cs
+++ UsrYacht/Files/obj/Debug/UsrYacht.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsrYacht")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8864bcdad6357f9336c3e97670d2e2a2975a9339")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsrYacht")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsrYacht")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
